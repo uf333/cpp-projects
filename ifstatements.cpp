487851fd50b6ceb7ba9702f626cdd1ca73d92494
Umm E Farwa <ummef333@gmail.com>
2026-05-28 18:47:44 -0400

    if else statements

---
+++ ifstatements.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std; 
+
+int main () {
+    int age; 
+    cout << "Enter your age:";
+    cin >> age; 
+
+if (age >= 18) {
+cout << "You are an adult" << endl; 
+} else {
+    cout << "You are a minor"<< endl; 
+}
+
+return 0; 
+
+} 
