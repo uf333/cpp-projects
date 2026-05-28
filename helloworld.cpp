956a1e7e2bb0f2a14f12669974eeb173bbf6ab41
Umm E Farwa <ummef333@gmail.com>
2026-05-28 16:44:53 -0400

    interactive hello world with user input

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-using namespace std ;
+#include <string>
+using namespace std; 
 
 int main() {
-    cout << "Hello World"<< endl; 
+    string name= ""; 
+    cout << "What's your name";
+    cin >> name; 
+    cout << "Hello" << name << "!" <<endl; 
     return 0; 
-    
-} 
+}
 
