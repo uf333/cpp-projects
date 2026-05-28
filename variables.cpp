60c469361c9df3b7047f0d8c38d8420d4eb49af1
Umm E Farwa <ummef333@gmail.com>
2026-05-28 16:55:56 -0400

    variables and data types

---
+++ variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string> 
+using namespace std; 
+
+int main () {
+    int age = 20; 
+    string name = "Umm E Farwa"; 
+    double gpa = 3.5; 
+    bool isStudent = true; 
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "GPA: " << gpa << endl;
+    cout << "Student: " << isStudent << endl;
+    
+
+return 0; 
+}
